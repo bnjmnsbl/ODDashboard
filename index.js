1c8218224eb1b86f5707fdc4b9ed3cf87b0d22d6
Benjamin <benjamin.seibel@gmx.de>
2018-04-28 23:37:00 +0200

    some additions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-var resultsArray = [];
+//var resultsArray = [];
 
 function jsonp(url) {
     return new Promise(function(resolve, reject) {
@@ -25,7 +25,7 @@ var data2 = jsonp("https://ckan.govdata.de/api/3/action/package_search?fq=organi
 
 Promise.all([data, data2])
 .then(values => {
-	resultsArray = values["0"].result.results.concat(values["1"].result.results);
+	var resultsArray = values["0"].result.results.concat(values["1"].result.results);
 	return resultsArray;
 })
 .then(data => {
@@ -33,8 +33,116 @@ Promise.all([data, data2])
 });
 
 function initialize(results) {
-	console.log("starting out!");
-	console.dir(results)
+
+	var licenses = getHighscores(results, "license", 3);
+	var authors = getHighscores(results, "author", 3);
+	var tags = getHighscores(results, "tags", 5);
+	var formats = getHighscores(results, "formats", 7);
+    console.dir(results);
+  	console.log("Total # of datasets: " + getTotalNumber(results))
+   	console.log("Neuester Datensatz: " + findNewest(results)); 
+	console.dir(getTimestamps(results));
+	console.dir(licenses);
+	console.dir(tags);
+	console.dir(formats);
+	//Namen zählen
+	//Häufigste Tags
+	// Anzahl Formate (in data.result.resources)
+
+
 }
+
+
+function findNewest(data) {
+	return data["0"].name;
+}
+
+function getTotalNumber(data) {
+	return data.length;
+}
+
+function getTimestamps(data) {
+	var timestamps = data.map(x => ({"time": x.metadata_modified, "name": x.name}));
+	return timestamps;
+}
+
+function getHighscores(data, item, amount){
+	
+	var countsObj = {};
+
+	if (item === "author") {
+
+		for (var i = 0; i < data.length; i++) {
+			if (countsObj.hasOwnProperty(data[i].author)) {
+				countsObj[data[i].author]++
+			}
+
+			else {
+				countsObj[data[i].author] = 1;
+			}
+
+		}
+	} else if (item === "license") {
+		for (var i = 0; i < data.length; i++) {
+			if (countsObj.hasOwnProperty(data[i].license_title)) {
+				countsObj[data[i].license_title]++
+			}
+
+			else {
+				countsObj[data[i].license_title] = 1;
+			}
+
+		}
+
+	} else if (item === "tags") {
+		for (var i = 0; i < data.length; i++) {
+
+			data[i].tags.forEach(function(el) {
+				
+				if (countsObj.hasOwnProperty(el.name)) {
+				
+				countsObj[el.name]++
+				
+				} else {
+				
+				countsObj[el.name] = 1;
+				
+				}
+
+			});
+
+		}
+
+	} else if (item === "formats") {
+		for (var i = 0; i < data.length; i++) {
+
+			data[i].resources.forEach(function(el) {
+				
+				if (countsObj.hasOwnProperty(el.format)) {
+				
+				countsObj[el.format]++
+				
+				} else {
+				
+				countsObj[el.format] = 1;
+				
+				}
+
+			});
+
+		}	
+	}
+
+		var props = Object.keys(countsObj).map(function(key) {
+			return {key: key, value: this[key] };
+		}, countsObj);
+
+		props.sort(function(p1, p2) {return p2.value - p1.value});
+		var tops = props.slice(0, amount);
+
+		return tops;
+}
+
+
 
 
